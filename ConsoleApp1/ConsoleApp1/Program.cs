42f0355c6b8063ff8a54587ae71781c93f2ed35d
prisalvarado18 <priscila.alvarado@studentambassadors.com>
2023-08-31 11:09:22 -0500

    Added first exercise

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Crear un programa que calcule el área del rectángulo
+double ladoA;
+double ladoB;
+double resultado;
+
+Console.WriteLine("Ingrese el lado A: ");
+ladoA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingrese el lado B: ");
+ladoB = Convert.ToDouble(Console.ReadLine());
+
+resultado = ladoA * ladoB;
+Console.WriteLine("Resultado: " + resultado);
